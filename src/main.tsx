4ef71d8cd9f230b6ca0fa4b1d0a72e480d2f8253
Winney <tatakornaow@gmail.com>
2024-03-10 15:31:36 +0700

    Consturction box

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/Info";
-import "./index.css";
+import App from "./components/Game/Game";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
