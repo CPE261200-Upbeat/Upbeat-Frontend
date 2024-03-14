263d684e6d627909e327d3f4dbe4a1406ec870ac
fdsdsfdsf <NestZ671@gmail.com>
2024-03-14 11:29:14 +0700

    local storage update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import webSocketReducer from "./slices/websocket";
 import gameReducer from "./slices/game";
-import credentialReducer from './slices/credential'
+import credentialReducer from "./slices/credential";
+
 export const store = configureStore({
   reducer: {
     game: gameReducer,
     webSocket: webSocketReducer,
-    credential : credentialReducer
+    credential: credentialReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -16,5 +17,6 @@ export const store = configureStore({
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
