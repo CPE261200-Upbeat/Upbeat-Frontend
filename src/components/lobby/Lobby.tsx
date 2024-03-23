27d4b213ec062e788493e3c7c4cb8c07d8c851cc
__.flxk.__ <nitipong4434@gmail.com>
2024-03-24 02:23:58 +0700

    Added a button to change user profile color.

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -1,25 +1,29 @@
 import "../lobby/Lobby.css";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useAppSelector } from "@/redux/hook";
+import { selectGame } from "@/redux/slices/game";
+import { selectPlayer } from "@/redux/slices/player";
 import useWebSocket from "@/websocket/useWebsocket";
 import { GameInfo } from "@/model/game";
-import { selectGame } from "@/redux/slices/game";
-import { useAppSelector } from "@/redux/hook";
-import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
-import { selectPlayer } from "@/redux/slices/player";
-import { Player } from "@/model/player";
 import { BEGIN_STATE } from "../Game/config/constant";
+import { Player } from "@/model/player";
+import { GameState } from "@/model/gameState";
+import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
+import { IoMdColorPalette } from "react-icons/io";
+import { HuePicker } from "react-color";
 
 function Lobby() {
   const navigate = useNavigate();
   const websocket = useWebSocket();
   const [isJoined, setIsJoined] = useState(false);
-
   const gameInfo: GameInfo = useAppSelector(selectGame);
   const players: Player[] = gameInfo.players.list;
   const player: Player = useAppSelector(selectPlayer);
   const isFirstPlayer =
     JSON.stringify(player?.acct) === JSON.stringify(players[0]?.acct);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [isHuePickerOpen, setIsHuePickerOpen] = useState(false);
 
   const handleLogout = () => {
     websocket.handleDisconnect(player);
@@ -46,6 +50,14 @@ function Lobby() {
     }
   };
 
+  const handleColorChange = (color: any) => {
+    setSelectedColor(color.hex);
+  };
+
+  const handleColorPickerTrigger = () => {
+    setIsHuePickerOpen(!isHuePickerOpen);
+  };
+
   return (
     <section>
       <div className="wappper_Lobby">
@@ -54,11 +66,32 @@ function Lobby() {
         <div className="player_wrapper">
           {gameInfo.players.list.map((player) => (
             <div key={player.acct.username} className="player">
-              <div>
+              <div className="user_container">
+                {isHuePickerOpen &&
+                  player.acct.username === player.acct.username && (
+                    <div className="color-picker-container">
+                      <HuePicker
+                        color={selectedColor || "#ffffff"}
+                        width="200px"
+                        height="15px"
+                        onChange={handleColorChange}
+                      />
+                    </div>
+                  )}
                 <FaUserCircle
                   className="user"
-                  color={`hsl(${player.color}, 100%, 50%)`}
+                  color={
+                    player.acct.username === player.acct.username
+                      ? selectedColor || `hsl(${player.color}, 100%, 50%)`
+                      : `hsl(${player.color}, 100%, 50%)`
+                  }
                 />
+                {player.acct.username === player.acct.username && (
+                  <IoMdColorPalette
+                    className="ColorPalette"
+                    onClick={handleColorPickerTrigger}
+                  />
+                )}
               </div>
               <button>{player.acct.username}</button>
             </div>
