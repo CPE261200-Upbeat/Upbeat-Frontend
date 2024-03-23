c2bf18b09d045fe570f14fb37bba9e24d1914c8b
__.flxk.__ <nitipong4434@gmail.com>
2024-03-23 10:13:55 +0700

    Improved user display when pressing the join button.

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -7,6 +7,8 @@ import { GameInfo } from "@/model/game";
 import { selectGame } from "@/redux/slices/game";
 import { useAppSelector } from "@/redux/hook";
 import { GameState } from "@/model/gameState";
+import { FaUserCircle } from "react-icons/fa";
+import { CiLogout } from "react-icons/ci";
 
 function Lobby() {
   const navigate = useNavigate();
@@ -16,7 +18,11 @@ function Lobby() {
   );
 
   const gameInfo: GameInfo = useAppSelector(selectGame);
-  
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   useEffect(() => {
     if (gameInfo.gameState?.isBegin) {
       navigate("/game");
@@ -31,7 +37,7 @@ function Lobby() {
         isOver: 0,
         isBegin: 1,
         isPaused: 0,
-        isError : 0,
+        isError: 0,
         turnCount: 1,
       };
       websocket.handleSetState(gameState);
@@ -42,36 +48,18 @@ function Lobby() {
 
   return (
     <section>
-      <div className="wappper">
+      <div className="wappper_Lobby">
+        <CiLogout className="CiLogout" onClick={handleLogout} />
         <h1>UPBEAT</h1>
-        <div className="player">
-          <div className="player_1">
-            <div>
-              <img src="/src/assets/material/profile.png" />
+        <div className="player_wrapper">
+          {gameInfo.players.list.map((player) => (
+            <div className="player">
+              <div>
+                <FaUserCircle className="user" color="rgb(0, 0, 0)" />
+              </div>
+              <button>{player.acct.username}</button>
             </div>
-            <button>player 1</button>
-          </div>
-
-          <div className="player_2">
-            <div>
-              <img src="/src/assets/material/blueprofile.png" />
-            </div>
-            <button>player 2</button>
-          </div>
-
-          <div className="player_3">
-            <div>
-              <img src="/src/assets/material/greyprofile.png" />
-            </div>
-            <button>player 3</button>
-          </div>
-
-          <div className="player_4">
-            <div>
-              <img src="/src/assets/material/yellowprofile.png" />
-            </div>
-            <button>player 4</button>
-          </div>
+          ))}
         </div>
         <div className="button">
           <button type="submit" onClick={() => handleClick("start")}>
