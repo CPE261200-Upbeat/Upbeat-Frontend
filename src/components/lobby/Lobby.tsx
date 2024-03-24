555ee5f4685b1cdb66abe060bc4696c792926d7a
__.flxk.__ <nitipong4434@gmail.com>
2024-03-24 11:20:55 +0700

    Edit color selection

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -6,7 +6,6 @@ import { selectGame } from "@/redux/slices/game";
 import { selectPlayer } from "@/redux/slices/player";
 import useWebSocket from "@/websocket/useWebsocket";
 import { GameInfo } from "@/model/game";
-import { BEGIN_STATE } from "../Game/config/constant";
 import { Player } from "@/model/player";
 import { GameState } from "@/model/gameState";
 import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
@@ -17,16 +16,13 @@ function Lobby() {
   const navigate = useNavigate();
   const websocket = useWebSocket();
   const [isJoined, setIsJoined] = useState(false);
-  const gameInfo: GameInfo = useAppSelector(selectGame);
-  const players: Player[] = gameInfo.players.list;
-  const player: Player = useAppSelector(selectPlayer);
-  const isFirstPlayer =
-    JSON.stringify(player?.acct) === JSON.stringify(players[0]?.acct);
-  const [selectedColor, setSelectedColor] = useState(null);
+  const [isFirstPlayer, setIsFirstPlayer] = useState(false);
+  const [selectedColorHSL, setSelectedColorHSL] = useState(null);
   const [isHuePickerOpen, setIsHuePickerOpen] = useState(false);
+  const gameInfo: GameInfo = useAppSelector(selectGame);
+  const currentPlayer: Player = useAppSelector(selectPlayer);
 
   const handleLogout = () => {
-    websocket.handleDisconnect(player);
     navigate("/login");
   };
 
@@ -34,28 +30,46 @@ function Lobby() {
     if (gameInfo.gameState?.isBegin) {
       navigate("/game");
     }
-  }, [gameInfo]);
+    if (isJoined && gameInfo.players.list.length === 1) {
+      setIsFirstPlayer(true);
+    } else {
+      setIsFirstPlayer(false);
+    }
+  }, [gameInfo, isJoined]);
 
   const handleClick = (buttonType: string) => {
     if (buttonType === "join") {
-      websocket.handleJoin(player);
+      websocket.handleJoin(currentPlayer);
       setIsJoined(true);
     } else if (buttonType === "disconnect") {
-      websocket.handleDisconnect(player);
       setIsJoined(false);
-    } else if (buttonType === "start") {
-      websocket.handleSetState(BEGIN_STATE);
+    } else if (buttonType === "start" && isFirstPlayer) {
+      const gameState: GameState = {
+        isOver: 0,
+        isBegin: 1,
+        isPaused: 0,
+        isError: 0,
+        turnCount: 1,
+      };
+      websocket.handleSetState(gameState);
     } else {
       console.error("Unexpected button type:", buttonType);
     }
   };
 
   const handleColorChange = (color: any) => {
-    setSelectedColor(color.hex);
+    setSelectedColorHSL(color.hsl);
   };
 
-  const handleColorPickerTrigger = () => {
+  const [activeColorPickerUser, setActiveColorPickerUser] = useState(null);
+
+  const handleColorPickerTrigger = (username: any) => {
+    setActiveColorPickerUser(
+      username === activeColorPickerUser ? null : username
+    );
     setIsHuePickerOpen(!isHuePickerOpen);
+    console.log(!isHuePickerOpen);
+    console.log(selectedColorHSL);
   };
 
   return (
@@ -67,11 +81,11 @@ function Lobby() {
           {gameInfo.players.list.map((player) => (
             <div key={player.acct.username} className="player">
               <div className="user_container">
-                {isHuePickerOpen &&
-                  player.acct.username === player.acct.username && (
+                {activeColorPickerUser === player.acct.username &&
+                  currentPlayer.acct.username === player.acct.username && (
                     <div className="color-picker-container">
                       <HuePicker
-                        color={selectedColor || "#ffffff"}
+                        color={selectedColorHSL || "#ffffff"}
                         width="200px"
                         height="15px"
                         onChange={handleColorChange}
@@ -81,15 +95,17 @@ function Lobby() {
                 <FaUserCircle
                   className="user"
                   color={
-                    player.acct.username === player.acct.username
-                      ? selectedColor || `hsl(${player.color}, 100%, 50%)`
+                    currentPlayer.acct.username === player.acct.username
+                      ? selectedColorHSL || `hsl(${player.color}, 100%, 50%)`
                       : `hsl(${player.color}, 100%, 50%)`
                   }
                 />
-                {player.acct.username === player.acct.username && (
+                {currentPlayer.acct.username === player.acct.username && (
                   <IoMdColorPalette
                     className="ColorPalette"
-                    onClick={handleColorPickerTrigger}
+                    onClick={() =>
+                      handleColorPickerTrigger(player.acct.username)
+                    }
                   />
                 )}
               </div>
